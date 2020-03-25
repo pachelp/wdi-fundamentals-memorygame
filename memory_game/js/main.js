1c7e22ae5f44439a58133c52d0d4786fe8158264
pachelp <62486372+pachelp@users.noreply.github.com>
2020-03-25 00:35:17 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and Running!");
+let cardOne = "Queen";
+let cardTwo = "Queen";
+let cardThree = "King";
+let cardFour = "King";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
