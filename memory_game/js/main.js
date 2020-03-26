a7d81a854d98ac09214c3170988ff0de162d9f44
pachelp <62486372+pachelp@users.noreply.github.com>
2020-03-26 01:49:02 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,42 @@
-console.log("Up and Running!");
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+ const cards = [
+ {
+ 	rank: 'queen',
+ 	suit: 'heart',
+ 	cardImage: "images/queen-of-hearts.png"
+},
+{
+ 	rank: 'queen',
+ 	suit: 'diamonds',
+ 	cardImage: "images/queen-of-diamonds.png"
+},
+{
+ 	rank: 'king',
+ 	suit: 'hearts',
+ 	cardImage: "images/king-of-hearts.png"
+},
+{
+ 	rank: 'king',
+ 	suit: 'diamonds',
+ 	cardImage: "images/king-of-diamonds.png"
+},
+] ;
+ const cardsInPlay = [] ; 
+ function checkForMatch () {
+ }
+ function flipCard(cardId) {
+ 	console.log("User flipped a" + " " + cards[cardId].rank);
+ 	console.log(cards[cardId].suit);
+ 	console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
+};
+
+flipCard (0);
+flipCard (2);
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  alert("Sorry, try again.");
+ };
+
+
